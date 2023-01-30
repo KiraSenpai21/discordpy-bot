552085c81c3c4a0fc394f29dd8df06dd83cfe08c
KiraSenpai <marko-pershqefa@hotmail.gr>
2023-01-30 15:03:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,165 +1,170 @@
-import os
 import discord
-from discord.ext import commands
-
-intents = discord.Intents.default()
-intents.members = True
-
-client = commands.Bot(command_prefix='$', intents=intents)
-
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        embed = discord.Embed(title="Error",
-                              description="Command not found",
-                              colour=discord.Colour.red())
-        await ctx.send(embed=embed)
-      
-@client.event
-async def on_ready():
-    print('Bot is now initialized... and online', f"latency: {round(client.latency * 1000)}ms")
-    try:
-        for filename in os.listdir("./cogs"):
-          if filename.endswith(".py") and filename != "__init__.py":
-             await client.load_extension(f'cogs.{filename[:-3]}')
-    except Exception as e:
-        print(e)
-
-@client.event
-async def on_member_join(member, reason=None):
-    embed = discord.Embed(
-      title = "Welcome To Unbroken!",
-      colour = discord.Colour.red()
-    )
-    embed.set_author(name=f"{member.name}")
-    embed.set_thumbnail(url=member.avatar_url)
-    await member.send(embed=embed)
-    role = 833336572749414420
-    eune = 828880287705595954
-    await member.add_roles((member.guild.get_role(role)), reason=reason)
-    await member.add_roles((member.guild.get_role(eune)), reason=reason)
-  
-# Bot Commands #
-
-# @client.command()
-# @commands.has_permissions(manage_messages=True)
-# async def clear(ctx, amount=5):
-#     """clears the chat """
-#     try:
-#         await ctx.channel.purge(limit=amount + 1)
-#         embed = discord.Embed(title=f"Cleared {amount} messages!",
-#                               colour=discord.Colour.green())
-#         await ctx.send(embed=embed)
-#     except None:
-#         pass
+from discord import app_commands
+from riotwatcher import LolWatcher
+from getChampionNameByID import get_champions_name
+import random
+import time
+import os
 
 
-@client.command()
-async def help(ctx):
-    embed = discord.Embed(
-        title="Bot Commands",
-        description="This are the Commands for the Unbroken Bot Prefix ( $ )",
-        colour=discord.Colour.green())
-    embed.set_footer(text="UnBroken Bot 2021")
-    embed.set_author(
-        name="UnBroken",
-        icon_url=
-        "https://lolstatic-a.akamaihd.net/frontpage/apps/prod/mid-season-magic/en_GB/4e9063b04dcba600ab1718f8af28dba18e380220/assets/img/elemental-dragon-fire-thumb.png"
-    )
-    embed.add_field(
-        name="League Of Legends Comamnds:",
-        value=
-        "$profile {summoner name} \n $register {summoner name} \n $me \n $delete_acc {summoner name}"
-    )
-    embed.add_field(
-        name="Admin Commands:",
-        value=
-        "$kick {user} \n $ban {user} \n $ping \n $load {extension} \n $reload {extension} \n $unload {extesion}"
-    )
-    embed.add_field(name="Music Commands:",
-                    value="$play {song name} \n $pause \n $skip \n $next ")
-    await ctx.send(embed=embed)
+class Unbroken(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.all())
+        self.synced = False
 
+    async def on_ready(self):
+        await tree.sync(guild=discord.Object(id=821479008574242918))
+        self.synced = True
+        await client.change_presence(status=discord.Status.dnd, activity=discord.Game('Unbroken!'))
+        print('Bot is now initialized... and online', f"latency: {round(client.latency * 1000)}ms")
+        # for filename in os.listdir("./cogs"):
+        #     if filename.endswith(".py") and filename != "__init__.py":
+        #         client.load_extension(f'cogs.{filename[:-3]}')
+                
 
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def ping(ctx):
+client = Unbroken()
+tree = app_commands.CommandTree(client)
+
+# Application commands
+@tree.command(name='ping', description='the ping the bot has with the server', guild=discord.Object(id=821479008574242918))
+async def ping(ctx:discord.Interaction):
     """Shows the Latency between the server and the bot"""
     embed = discord.Embed(title=f"latency: {round(client.latency * 1000)}ms",
                           colour=discord.Colour.green())
-    await ctx.send(embed=embed)
+    await ctx.response.send_message(embed=embed)
 
+# League
 
-@client.command()
-@commands.has_permissions(administrator=True)
-async def load(ctx, extension):
-    """Loads Extensions"""
-    client.load_extension(f"cogs.{extension}")
-    embed = discord.Embed(title=f"( {extension} ) Loaded.",
-                          colour=discord.Colour.green())
-    await ctx.send(embed=embed)
+@tree.command(name='profile', description='shows stats about your rank in this season', guild=discord.Object(id=821479008574242918))
+async def profile(ctx:discord.Interaction, username:str):
+      prefix = '$'
+      version = '12.8'
+      region = 'eun1'
+      API = os.environ['LApi']
+      lol_watcher = LolWatcher(API)
+      me = lol_watcher.summoner.by_name(region, username)
+      ranked_stats = lol_watcher.league.by_summoner(region, me['id'])
+      lvl = me["summonerLevel"]
+      code = me['profileIconId']
+      id = me["id"]
+      champs = lol_watcher.champion_mastery.by_summoner(region,id)
+      championLevel = champs[0]['championLevel']
+      championId = champs[0]['championId']
+      championPoints = "{:,}".format(champs[0]['championPoints'])
+      if ranked_stats[0]["queueType"] == 'RANKED_TFT_PAIRS':
+        if len(ranked_stats) == 3:
+          if ranked_stats[1]["queueType"] == 'RANKED_FLEX_SR':
+              flexed = ranked_stats[1]  
+              ranked = ranked_stats[2]
+          else:
+              flexed = ranked_stats[2]
+              ranked = ranked_stats[1]
+        elif len(ranked_stats) == 2:
+          if ranked_stats[1]["queueType"] == 'RANKED_SOLO_5x5':
+              ranked = ranked_stats[1]
+              flexed = []
+          else:
+              flexed = ranked_stats[1]
+              ranked = []
+      else:
+        if len(ranked_stats) == 2:
+          if ranked_stats[0]["queueType"] == 'RANKED_FLEX_SR':
+              flexed = ranked_stats[0]  
+              ranked = ranked_stats[1]
+          else:
+              flexed = ranked_stats[1]
+              ranked = ranked_stats[0]
+        elif len(ranked_stats) == 1:
+          if ranked_stats[0]["queueType"] == 'RANKED_SOLO_5x5':
+              ranked = ranked_stats[0]
+              flexed = []
+          else:
+              flexed = ranked_stats[0]
+              ranked = []
+#KATHOLOU RANKED
+      if len(ranked_stats) == 0:
+          embed = discord.Embed(
+              title=f"Level: {lvl}",
+              description=
+              f"Solo/Duo Wins: Unranked \n Flex Wins: Unranked",
+              colour=discord.Colour.green())
+          embed.add_field(name="Solo/Duo",
+                          value=f"Unranked",
+                          inline=True)
+          embed.add_field(name="Flex",
+                          value=f"Unranked",
+                          inline=True)
+          embed.set_author(
+              name=f"{me['name']}",
+              icon_url=
+              f"https://raw.communitydragon.org/latest/game/assets/ux/summonericons/profileicon{code}.png"
+          )
+          await ctx.response.send_message(embed=embed)
+#MONO SOLO/DUO
+      elif flexed == []:
+          tier = ranked["tier"]
+          rank = ranked["rank"]
+          wins = ranked["wins"]
+          losses = ranked["losses"]
+          lp = ranked["leaguePoints"]
+          embed = discord.Embed(
+              title=f"Level: {lvl}",
+              description=
+              f"Solo/Duo Wins: **{wins}** Losses: **{losses}** Wins behind or ahead: **{wins - losses}**  \n Flex Wins: No wins or losses",
+              colour=discord.Colour.purple())
+          embed.add_field(
+              name="Solo/Duo",
+              value=
+              f"Tier: {tier} \n Rank: {rank} \n LP: {lp} \n Total Games: **{wins + losses}**",
+              inline=True)
+          embed.add_field(
+              name=f"Main's {get_champions_name(championId)}",
+              value=f"Mastery Level: {championLevel} \n Points: {championPoints}",
+              inline=True
+          )
+          embed.set_author(
+              name=f"{me['name']}",
+              icon_url=
+              f"https://raw.communitydragon.org/latest/game/assets/ux/summonericons/profileicon{code}.png"
+          )
+          rank_icon = str.lower(tier)
+          embed.set_thumbnail(
+              url=
+              f"https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-shared-components/global/default/{rank_icon}.png"
+          )
+          await ctx.response.send_message(embed=embed)
+#MONO FLEX
+      elif ranked == []:
+          tier = flexed["tier"]
+          rank = flexed["rank"]
+          wins = flexed["wins"]
+          losses = flexed["losses"]
+          lp = flexed["leaguePoints"]
+          embed = discord.Embed(
+              title=f"Level: {lvl}",
+              description=
+              f"Solo/Duo No Wins or Losses  \n Flex Wins: **{wins}** Losses: **{losses}** Wins behind or ahead: **{wins - losses}**",
+              colour=discord.Colour.purple())
+          embed.add_field(
+              name="Flex",
+              value=
+              f"Tier: {tier} \n Rank: {rank} \n LP: {lp} \n Total Games: **{wins + losses}**",
+              inline=True)
+          embed.add_field(
+              name=f"Main's {get_champions_name(championId)}",
+              value=f"Mastery Level: {championLevel} \n Points: {championPoints}",
+              inline=True
+          )
+          embed.set_author(
+              name=f"{me['name']}",
+              icon_url=
+              f"https://raw.communitydragon.org/latest/game/assets/ux/summonericons/profileicon{code}.png"
+          )
+          rank_icon = str.lower(tier)
+          embed.set_thumbnail(
+              url=
+              f"https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-shared-components/global/default/{rank_icon}.png"
+          )
+          await ctx.response.send_message(embed=embed)
 
-
-@client.command()
-@commands.has_permissions(administrator=True)
-async def unload(ctx, extension):
-    """Unloads extensions"""
-    client.unload_extension(f"cogs.{extension}")
-    embed = discord.Embed(title=f"( {extension} ) unloaded Successfully.",
-                          colour=discord.Colour.green())
-    await ctx.send(embed=embed)
-
-
-@client.command()
-@commands.has_permissions(administrator=True)
-async def reload(ctx, extension):
-    """Reloads existing Extensions"""
-    client.reload_extension(f"cogs.{extension}")
-    embed = discord.Embed(title=f"( {extension} ) Reloaded Successfully.",
-                          colour=discord.Colour.green())
-    await ctx.send(embed=embed)
-
-
-@reload.error
-async def handler_reload(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        if error.param.name == 'extension':
-            embed = discord.Embed(
-                title="Error",
-                description="You forgot to give the name of the extesion",
-                colour=discord.Colour.red())
-            await ctx.send(embed=embed)
-    raise error
-
-
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def unmute(ctx, member: discord.Member):
-    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
-    await member.remove_roles(mutedRole)
-    embed = discord.Embed(title="Unmuted",
-                          description=f" unmuted-{member.mention}",
-                          color=discord.Colour.green())
-    await ctx.send(embed=embed)
-
-
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def mute(ctx, member: discord.Member, *, reason=None):
-    guild = ctx.guild
-    mutedRole = discord.utils.get(guild.roles, name="Muted")
-    if not mutedRole:
-        mutedRole = await guild.create_role(name="Muted")
-        for channel in guild.channels:
-            await channel.set_permissions(mutedRole,
-                                          speak=False,
-                                          send_messages=False,
-                                          read_message_history=True,
-                                          read_messages=False)
-    embed = discord.Embed(title="Muted",
-                          description=f"{member} was muted ",
-                          color=discord.Colour.green())
-    embed.add_field(name="reason:", value=reason, inline=False)
-    await ctx.send(embed=embed)
-    await member.add_roles(mutedRole, reason=reason)
 client.run(os.environ["DISCORD_TOKEN"])
